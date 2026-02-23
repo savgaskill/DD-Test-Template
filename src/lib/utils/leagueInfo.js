514aa5494df9af6e100a6845920c5f07513f0108
savgaskill <savgaskill@gmail.com>
2026-02-23 15:41:06 -0600

    Update league ID and name in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "1313137559294988288"; // your league ID
-export const leagueName = "Dynasty Dads"; // your league name
+export const leagueID = "1314643735715274752"; // your league ID
+export const leagueName = "Dynasty Dads Mock Startup"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
